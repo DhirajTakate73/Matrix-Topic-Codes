4a132a4e7dcb0a2872a6c60446b78ffd261fd2b1
Dhiraj M Takate <dhirajtakate73@gmail.com>
2024-02-10 15:36:53 +0530

    Add files via upload

--- Program403.java
+++ Program403.java
@@ -8,23 +8,139 @@ class MyMatrix extends Matrix
         super(A,B);
     }
 
-    public int CountEven()
+    public void SummationRow()
     {
-        int i = 0, j = 0;
-        int iFrequency = 0;
+        int i = 0, j = 0, isum = 0;
 
-        for(i = 0 ; i < Arr.length; i++)
+        for(i = 0; i < arr.length; i++)        
         {
-            for(j = 0 ; j < Arr[i].length; j++)
+            for(j = 0; j < arr[i].length; j++)
             {
-                if((Arr[i][j] % 2) == 0)
+                isum = arr[i][j] + isum;
+            }
+            System.out.println("Summation of Row : "+(i+1)+" is : "+isum);
+            isum = 0;
+        }
+    }
+
+    public void MaximumRow()
+    {
+        int imax = 0, i = 0, j = 0 ;
+
+        for(i = 0; i < arr.length; i++)        
+        {
+            imax = arr[i][0];
+
+            for(j = 0; j < arr[i].length; j++)
+            {
+                if(arr[i][j] > imax)
                 {
-                    iFrequency++;
+                    imax = arr[i][j];
                 }
+                
+            }
+            System.out.println("maximum element of Row : "+(i+1)+" is : "+imax);
+        }
+
+    }
+
+    public void MinimumRow()
+    {
+        int imin = 0, i = 0, j = 0 ;
+
+        for(i = 0; i < arr.length; i++)        
+        {
+            imin = arr[i][0];
+
+            for(j = 0; j < arr[i].length; j++)
+            {
+                if(arr[i][j] < imin)
+                {
+                    imin = arr[i][j];
+                }
+                
+            }
+            System.out.println("minimum element of Row : "+(i+1)+" is : "+imin);
+        }
+
+    }
+
+    public int frequency(int ino)
+    {
+        int i = 0, j = 0, ifrequency = 0;
+
+        for(i = 0; i < arr.length; i++)
+        {
+            for(j = 0; j < arr[i].length; j++)
+            {
+                if(arr[i][j] == ino)
+                {
+                    ifrequency++;
+                }
+
             }
         }
-        return iFrequency;
+        return ifrequency;
+        
     }
+
+    public int counteven()
+    {
+        int i = 0, j = 0, icount = 0;
+
+        for(i = 0; i < arr.length; i++)
+        {
+            for(j = 0; j < arr[i].length; j++)
+            {
+                if((arr[i][j] % 2) == 0)
+                {
+                    icount++;
+                }
+
+            }
+        }
+        return icount;
+        
+    }
+
+    public int countodd()
+    {
+        int i = 0, j = 0, icount = 0;
+
+        for(i = 0; i < arr.length; i++)
+        {
+            for(j = 0; j < arr[i].length; j++)
+            {
+                if((arr[i][j] % 2) != 0)
+                {
+                    icount++;
+                }
+
+            }
+        }
+        return icount;
+        
+    }
+
+    public void Updatematrix()
+    {
+        int i = 0, j = 0;
+
+        for(i = 0; i < arr.length; i++)
+        {
+            for(j = 0; j < arr[i].length; j++)
+            {
+                if((arr[i][j] % 2) != 0)
+                {
+                    arr[i][j] = (arr[i][j]) + 1;
+                }
+
+            }
+        }
+        
+    }
+
+    
 }
 
 class Program403
@@ -32,7 +148,7 @@ class Program403
     public static void main(String Arg[])
     {
         Scanner sobj = new Scanner(System.in);
-        int iRow = 0, iCol = 0, iRet = 0;
+        int iRow = 0, iCol = 0, iRet = 0, ivalue = 0;
 
         System.out.println("Enter number of rows : ");
         iRow = sobj.nextInt();
@@ -43,13 +159,28 @@ class Program403
         MyMatrix mobj = new MyMatrix(iRow,iCol);
 
         mobj.Accept();
-        mobj.Display();
+        mobj.display();
+        mobj.SummationRow();
+        mobj.MaximumRow();
+        mobj.MinimumRow();
 
-        // Function calls
-        iRet = mobj.CountEven();
+        System.out.println("Enter the number that you want to search : ");
+        ivalue = sobj.nextInt();
 
-        System.out.println("Even elements are : "+iRet);
+        iRet = mobj.frequency(ivalue);
+        System.out.println("Frequency of Entered no. is : "+iRet);
 
+        iRet = mobj.counteven();
+        System.out.println("Count of even numbers is : "+iRet);
+
+        iRet = mobj.countodd();
+        System.out.println("Count of Odd numbers is : "+iRet);
+
+        mobj.Updatematrix();
+        System.out.println("Updated Matrix is : ");
+
+        mobj.display();
+        
         mobj = null;
         System.gc();
     }
